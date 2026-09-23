150cb6fe627349e8e0e390d094a1882b3645443c
JMRCycling <mtuft1@byui.edu>
2026-09-23 13:33:57 -0600

    Pre EJS

--- server.js
+++ server.js
@@ -1,21 +1,48 @@
-// Import express using ESM syntax
+// Imports
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// Create an instance of an Express application
+
+// Declare Important Variables
+// Define the port number the server will listen on
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Setup Express Server
 const app = express();
 
-const name = process.env.NAME;
-// Define a route handler for the root URL ('/')
+/**
+ * Configure Express middleware
+ */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-app.get('/new-route', (req, res) => {
-    res.send('This is a new route!');
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
 });
 
-// Define the port number the server will listen on
-const PORT = 3000;
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
+
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
